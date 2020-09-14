d9f696e9bdc941e561cd0ba26ec3cadaef29ec67
markomeje <markomejeonline@gmail.com>
2020-09-14 10:31:39 +0100

    Remove Documents

--- framework/Views/frontend/about/index.php
+++ framework/Views/frontend/about/index.php
@@ -55,23 +55,7 @@
 				<div class="col-12 col-md-10 col-lg-8 mb-4">
 					<h1 class="text-shade text-center">Our Legalities</h1>
 					<p class="text-muted text-center" data-aos="fade-up">We are officially registered and certified by the Federal Government of Nigeria and also approved by Economic and Financial Crime Commission (EFCC) under EFCC/SCUML against every illegal financial activities also attached with this, are certifications on our registrations.</p>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-12 col-md-4 mb-4">
-					<div class="">
-						<img src="<?= IMAGES_URL; ?>/documents/1.jpeg" class="img-fluid">
-					</div>
-				</div>
-				<div class="col-12 col-md-4 mb-4">
-					<div class="">
-						<img src="<?= IMAGES_URL; ?>/documents/2.jpeg" class="img-fluid">
-					</div>
-				</div>
-				<div class="col-12 col-md-4 mb-4">
-					<div class="">
-						<img src="<?= IMAGES_URL; ?>/documents/3.jpeg" class="img-fluid">
-					</div>
+					<p class="text-muted">To know more about our legalities with the government email us at <a href="mailto:admin@sustainedcare.org">admin@sustainedcare.org</a></p>
 				</div>
 			</div>
 		</div>
