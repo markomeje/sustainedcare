ed5d0ee909d798b6290d52809af8565b6e1e6c03
markomeje <markomejeonline@gmail.com>
2020-09-14 10:21:56 +0100

    Fixing Referral Title

--- framework/Controllers/HomeController.php
+++ framework/Controllers/HomeController.php
@@ -12,7 +12,8 @@ class HomeController extends Controller {
 
 	public function index($code = "") {
 		if ($code !== "") Cookie::set("applicant_referral_code", $code, time() + REFERRAL_LINK_CODE_EXPIRY, COOKIE_PATH, COOKIE_DOMAIN, COOKIE_SECURE, COOKIE_HTTP);
-		View::render("home/index", "frontend", ["title" => "We are ensuring that NO CHILD is deprived of their EDUCATIONAL RIGHTS and also creating various initiatives in order to promote a Poverty-Free World."]);
+		$title = ($code !== "") ? "Get registered today with Sustained Care Foundation and Access our Free On-going GRANTS for Existing and New Businesses up to a tune of NGN200,000 Only." : "We are ensuring that NO CHILD is deprived of their EDUCATIONAL RIGHTS and also creating various initiatives in order to promote a Poverty-Free World.";
+		View::render("home/index", "frontend", ["title" =>  $title]);
 	}
 
 }
