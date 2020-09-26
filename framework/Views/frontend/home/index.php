aa8d1bf598797be70e5b2286be681be978be5277
markomeje <markomejeonline@gmail.com>
2020-09-26 06:38:44 +0100

    Fix Spacing

--- framework/Views/frontend/home/index.php
+++ framework/Views/frontend/home/index.php
@@ -6,7 +6,7 @@
 				<div class="row justify-content-center text-center pt-5">
 					<div class="col-12 col-md-12 col-lg-8 pt-5 mb-4">
 						<h1 class="text-white font-weight-bolder pt-5 mb-4" data-aos="fade-up">Apply for <span class="text-orange">SPECIAL FREE GRANT</span> for Your Small Business or Project<span class="text-orange d-inline">.</span></h1>
-						<p class="text-white mb-4" data-aos="fade-up">Stand a chance to get UP TO A MAXIMUM of <span class="text-orange">NGN200,000 (Two Hundred Thousand Naira)</span> Grant for your choice of Business or Project.</p>
+						<p class="text-white mb-4" data-aos="fade-up">Stand a chance to get UP TO A MAXIMUM of <span class="text-orange"> NGN200,000 (Two Hundred Thousand Naira)</span> Grant for your choice of Business or Project.</p>
 						<div class="mb-4" data-aos="fade-up">
 							<a href="<?= DOMAIN; ?>/apply" class="btn btn-lg px-4 mr-4 orange-gradient text-white orange-shadow border-0 rounded-pill">Apply Now</a>
 						</div>
